470f245ff840593df1aa537e7c028fb9fcf4e541
xuejiaW <wxjwangxuejia@outlook.com>
2025-06-27 15:25:02 +0800

    feat: add Ch03_OpenXRCore
    Signed-off-by: xuejiaW <wxjwangxuejia@outlook.com>

---
+++ Ch03_OpenXRCore/app/src/main/cpp/Application/OpenXRTutorial.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <GraphicsAPI.h>
+#include <xr_linear_algebra.h>
+#include <openxr/openxr.h>
+
+#if defined(__ANDROID__)
+#include <android_native_app_glue.h>
+#endif
+
+class OpenXRTutorial
+{
+public:
+    OpenXRTutorial(GraphicsAPI_Type apiType);
+    ~OpenXRTutorial();
+
+    void Run();
+
+    static GraphicsAPI_Type m_apiType;
+
+#if defined(__ANDROID__)
+  public:
+    static android_app *androidApp;
+
+    struct AndroidAppState
+    {
+        ANativeWindow *nativeWindow = nullptr;
+        bool resumed = false;
+    };
+    static AndroidAppState androidAppState;
+
+    static void AndroidAppHandleCmd(struct android_app *app, int32_t cmd);
+#endif
+
+private:
+    void PollSystemEvents();
+};
